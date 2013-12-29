c4fb294e58b2244e5769d4288a163fccc9c5bf3b
Welles Robinson <wir963@gmail.com>
2013-12-28 21:47:20 -0600

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Welles Robinson on 12/28/13.
+//  Copyright (c) 2013 Welles Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Welles Robinson on 12/28/13.
+//  Copyright (c) 2013 Welles Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
